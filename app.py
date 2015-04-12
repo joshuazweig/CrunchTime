1cfd69eaf53e42406d951c8268d2aca37e955d6b
Joshua Zweig <joshuazweig@nat-tvwna-inside-visitornet101-b-29215.princeton.org>
2015-04-11 20:50:36 -0400

    WOrking but slow questions

--- app.py
+++ app.py
@@ -33,5 +33,67 @@ def get_random_company():
   company = companies_db.find().limit( -1 ).skip( int(random.random() * companies_db.count()) ).next()
   return company['p']
 
+
+@app.route("/question")
+def question():
+  
+  return questions(2)
+  # return "response"
+
+################################################################################
+
+
+
+def questions(questionNumber):
+  company = get_random_company()
+
+  payload = {
+             'user_key': user_key
+             }
+
+  r = requests.get("https://api.crunchbase.com/v/2/" + company, params=payload)
+  if(questionNumber == 1):
+
+    while 'founded_on_year' not in r.json()['data']['properties']:
+      company = get_random_company()
+      r = requests.get("https://api.crunchbase.com/v/2/" + company, params=payload)
+
+    question = "What year was " + company[13:] + " founded?" 
+    correct_response = r.json()['data']['properties']['founded_on_year']
+    response = [question, correct_response]
+    while len(response) < 5:
+      co = get_random_company()
+      new_r = requests.get("https://api.crunchbase.com/v/2/" + co, params=payload)
+      while 'founded_on_year' not in new_r.json()['data']['properties']:
+        co = get_random_company()
+        new_r = requests.get("https://api.crunchbase.com/v/2/" + co, params=payload)
+      possible_response = new_r.json()['data']['properties']['founded_on_year']
+      if correct_response != possible_response:
+          response.append(possible_response)
+
+      return jsonify(data=response)
+
+  if(questionNumber == 2):
+   
+    while 'total_funding_usd' not in r.json()['data']['properties']:
+      company = get_random_company()
+      r = requests.get("https://api.crunchbase.com/v/2/" + company, params=payload)
+
+    question = "How much funding has " + company[13:] + " recieved to date?" #get rid of org/
+    correct_response = r.json()['data']['properties']['total_funding_usd']
+    response = [question, correct_response]
+    while len(response) < 5:
+      co = get_random_company()
+      new_r = requests.get("https://api.crunchbase.com/v/2/" + co, params=payload)
+      while 'total_funding_usd' not in new_r.json()['data']['properties']:
+        co = get_random_company()
+        new_r = requests.get("https://api.crunchbase.com/v/2/" + co, params=payload)
+      possible_response = new_r.json()['data']['properties']['total_funding_usd']
+      if correct_response != possible_response:
+        response.append(possible_response)
+    return jsonify(data=response)
+
+
 if __name__ == "__main__":
-	app.run()
+  app.run()
+
